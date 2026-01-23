d26b3bd03b655ae045ea84476f959ab8738cf31f
kevchuang <kevin.chuang@ledger.fr>
2026-01-23 12:00:55 +0100

    refactor: Make def run output type more explicit

--- modules/core/src/main/scala/lumens/query/syntax.scala
+++ modules/core/src/main/scala/lumens/query/syntax.scala
@@ -7,5 +7,5 @@ import org.apache.lucene.search.IndexSearcher
 
 extension [C[_], A](intent: QueryIntent[C, A])
     /** Runs the query and returns the results in the given F. */
-    def run[F[_]](searcher: IndexSearcher)(using F: MonadError[F]): F[intent.Output] =
+    def run[F[_]](searcher: IndexSearcher)(using F: MonadError[F]): F[C[A]] =
         QueryRun[F](searcher).execute(intent)
